540e4a9985ed1fef9875a6c04d15b022dfc9c486
turtletop1 <ttmo04450@gmail.com>
2026-08-18 22:34:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -943,7 +943,7 @@ pdfjsLib.GlobalWorkerOptions.workerSrc =
 /* =====================================================
    CHECK JAVASCRIPT
 ===================================================== */
-alert("okok");
+
 console.log(
     "✅ app.js loaded successfully!"
 );
